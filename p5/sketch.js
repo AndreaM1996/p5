4fc5688f24549849bb1a741d9c166c2e959561ae
AndreaM1996 <32595514+AndreaM1996@users.noreply.github.com>
2019-02-02 12:22:24 +0100

    Add triangle

--- p5/sketch.js
+++ p5/sketch.js
@@ -6,10 +6,15 @@ function setup() {
 
   fill(0,170,0);
   rect(width/2-50, height/2-50, 100, 100);
-  
+
   fill(255);
   ellipse(width/2-50+25, height/2-50+30, 20, 20)
   ellipse(width/2-50+70, height/2-50+30, 30, 30)
 
   line(width/2-30, height/2+30, width/2+30, height/2+30)
+
+  translate(width/2-60,height/2-60);
+  fill(255,100,30);
+  triangle(0, 0, 0, 60, 60, 0);
+
 }
